e5f84652f93335d1da4961ec9d08159ce43470c6
FabrizioFavia <personal.favia@gmail.com>
2023-05-22 17:55:29 +0200

    feat(AppMain): added label and button

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,13 +9,22 @@ export default {
 <template>
     <main>
         <div class="mainPart">
-            <div class="container-lg p-5 d-flex justify-content-between flex-wrap">
-                <template v-for="comic in comics">
-                    <div class="comic">
-                        <img class="comicImg" :src="comic.thumb" alt="">
-                        <p class="mt-3 text-white">{{ comic.series.toUpperCase() }}</p>
-                    </div>
-                </template>
+
+            <div class="container-lg p-5">
+                <div class="currentS text-white p-2 d-flex justify-content-center">
+                    CURRENT SERIES
+                </div>
+                <div class="comicContainer d-flex justify-content-between flex-wrap">
+                    <template v-for="comic in comics">
+                        <div class="comic">
+                            <img class="comicImg" :src="comic.thumb" alt="">
+                            <p class="mt-3 text-white">{{ comic.series.toUpperCase() }}</p>
+                        </div>
+                    </template>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button class="label text-white mt-5 px-5">LOAD MORE</button>
+                </div>
             </div>
         </div>
     </main>
@@ -26,8 +35,22 @@ export default {
 
 .mainPart {
     width: 100%;
-    max-height: 680px;
-    background-color: $secondaryColor
+    min-height: 680px;
+    background-color: $secondaryColor;
+    position: relative;
+}
+
+.currentS {
+    width: 250px;
+    background-color: $primaryColor;
+    position: absolute;
+    top: -30px;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.label {
+    background-color: $primaryColor;
 }
 </style>
 
